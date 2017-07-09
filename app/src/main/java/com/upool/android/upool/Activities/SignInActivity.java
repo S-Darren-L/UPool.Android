cbfed28368d3d5c2368ebd1a44649c9ca49e83f6
S-Darren-Liu <gulang15@gmail.com>
2017-07-09 00:57:03 -0400

    ui update

--- app/src/main/java/com/upool/android/upool/Activities/SignInActivity.java
+++ app/src/main/java/com/upool/android/upool/Activities/SignInActivity.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -15,6 +12,8 @@ import com.upool.android.upool.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class SignInActivity extends AppCompatActivity {
 //    public static final String EXTRA_LOG_IN_EMAIL = "com.upool.android.upool.LOGINEMAIL";
@@ -33,32 +32,30 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         ButterKnife.bind(this);
+    }
 
-        emailText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+    @OnTextChanged(R.id.editEmail)
+    public void onEmailTextChanged() {
+        email = emailText.getText().toString();
+    }
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+    @OnTextChanged(R.id.editPassword)
+    public void onPasswordTextChanged() {
+        password = passwordText.getText().toString();
+    }
 
-            @Override
-            public void afterTextChanged(Editable editable) {
-                email = emailText.getText().toString();
-            }
-        });
+    @OnClick(R.id.buttonSignIn)
+    public void onSignInClick() {
+        Intent signInIntent = new Intent(this, VehicleRequestActivity.class);
+//        signInIntent.putExtra(EXTRA_LOG_IN_EMAIL, email);
+//        signInIntent.putExtra(EXTRA_LOG_IN_PASSWORD, password);
+        startActivity(signInIntent);
+    }
 
-        passwordText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                password = passwordText.getText().toString();
-            }
-        });
+    @OnClick(R.id.buttonSignUp)
+    public void onSignUpClick() {
+        Intent signUpIntent = new Intent(this, SignUpActivity.class);
+        startActivity(signUpIntent);
     }
 
     //Dismiss keyboard when touch outside of EditText
@@ -68,17 +65,5 @@ public class SignInActivity extends AppCompatActivity {
                 INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         return true;
-    }
-
-    public void SignIn(View view){
-        Intent signInIntent = new Intent(this, VehicleRequestActivity.class);
-//        signInIntent.putExtra(EXTRA_LOG_IN_EMAIL, email);
-//        signInIntent.putExtra(EXTRA_LOG_IN_PASSWORD, password);
-        startActivity(signInIntent);
-    }
-
-    public void SignUp(View view){
-        Intent signUpIntent = new Intent(this, SignUpActivity.class);
-        startActivity(signUpIntent);
     }
 }
